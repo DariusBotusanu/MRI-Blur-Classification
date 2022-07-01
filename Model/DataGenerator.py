21fd0836c2cd619d8bb4596699639b315c1b0f65
Darius Botusanu <botusanu.darius31@outlook.com>
2022-07-01 17:37:45 +0300

    Update DataGenerator.py

--- Model/DataGenerator.py
+++ Model/DataGenerator.py
@@ -27,6 +27,7 @@ class DataGenerator(keras.utils.Sequence):
 
     def __len__(self):
         'Denotes the number of batches per epoch'
+        # #sample/batch size
         return int(np.floor(len(self.list_IDs) / self.batch_size))
 
     def __getitem__(self, index):
@@ -37,7 +38,34 @@ class DataGenerator(keras.utils.Sequence):
         # x -> the cube of images 
         # y -> array of targets
         #randomize it 
-       # return X, y
+        # return X, y
+       
+        # Generate indexes of the batch
+        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+
+        # Find list of IDs
+        list_IDs_temp = [self.list_IDs[k] for k in indexes]
+
+        # Generate data
+        X, y = self.__data_generation(list_IDs_temp)
+
+        return X, y
+       
+    def __data_generation(self, list_IDs_temp):
+        'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
+        # Initialization
+        X = np.empty((self.batch_size, *self.dim, self.n_channels))
+        y = np.empty((self.batch_size), dtype=int)
+
+        # Generate data
+        for i, ID in enumerate(list_IDs_temp):
+            # Store sample
+            X[i,] = np.load(ID)
+
+            # Store class
+            y[i] = self.labels[ID]
+
+        return X, y
 
     def on_epoch_end(self):
         'Updates indexes after each epoch'
